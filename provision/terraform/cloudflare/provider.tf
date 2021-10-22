c1175f576a6fa415145a2252aab3dabb553eacb2
Christopher Larivière <lariviere.c@gmail.com>
2021-10-22 19:12:08 -0400

    add terraform for cloudflare

---
+++ provision/terraform/cloudflare/provider.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "3.3.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "0.6.3"
+    }
+  }
+}
+
+data "sops_file" "cloudflare_secrets" {
+  source_file = "secrets.sops.yaml"
+}
+
+provider "cloudflare" {
+  email   = data.sops_file.cloudflare_secrets.data["SECRET_EMAIL"]
+  api_key = data.sops_file.cloudflare_secrets.data["SECRET_CLOUDFLARE_API_KEY"]
+}
+
+data "cloudflare_zones" "domain" {
+  filter {
+    name = data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]
+  }
+}
---
+++ provision/terraform/cloudflare/records.tf
@@ -0,0 +1,21 @@
+data "http" "ipv4" {
+  url = "https://api.ipify.org"
+}
+
+resource "cloudflare_record" "ipv4" {
+  name    = data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = chomp(data.http.ipv4.body)
+  proxied = true
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "cname_wireguard" {
+  name    = "wireguard"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "${data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]}"
+  proxied = false
+  type    = "CNAME"
+  ttl     = 1
+}
