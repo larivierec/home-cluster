efeaf5e8db545680529bfbafb1eca367ac1566a5
Christopher Larivière <lariviere.c@gmail.com>
2021-11-22 18:25:08 -0500

    add 3 records. protecting domain spoofing.

--- provision/terraform/cloudflare/records.tf
+++ provision/terraform/cloudflare/records.tf
@@ -2,8 +2,8 @@ data "http" "ipv4" {
   url = "https://api.ipify.org"
 }
 
-resource "cloudflare_record" "ipv4" {
-  name    = "${data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]}"
+resource "cloudflare_record" "ip" {
+  name    = data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
   value   = chomp(data.http.ipv4.body)
   proxied = true
@@ -14,8 +14,33 @@ resource "cloudflare_record" "ipv4" {
 resource "cloudflare_record" "cname_wireguard" {
   name    = "wireguard"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "${data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]}"
+  value   = data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]
   proxied = false
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "mail_spf" {
+  name    = data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["SECRET_MX_DOMAIN_DATA"]
+  type    = "TXT"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "mail_dkey" {
+  name    = "*._domainkey"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["SECRET_MX_DOMAIN_KEY"]
+  type    = "TXT"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "mail_dmarc" {
+  name    = "_dmarc"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["SECRET_MX_DOMAIN_DMARC"]
+  type    = "TXT"
+  ttl     = 1
+}
+
