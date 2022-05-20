9ab41f677d1a095cccd99bc3ce35c5603f162123
Christopher Larivière <lariviere.c@gmail.com>
2022-05-20 18:53:43 -0400

    change mail provisions
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- provision/terraform/cloudflare/records.tf
+++ provision/terraform/cloudflare/records.tf
@@ -20,6 +20,33 @@ resource "cloudflare_record" "cname_wireguard" {
   ttl     = 1
 }
 
+
+resource "cloudflare_record" "mail_txt" {
+  name    = data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["SECRET_MX_DOMAIN_TXT_1"]
+  type    = "TXT"
+  ttl     = 300
+}
+
+resource "cloudflare_record" "mail_mx_1" {
+  name    = data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["SECRET_MX_DOMAIN_MX_1"]
+  type    = "MX"
+  ttl     = 300
+  priority = 10
+}
+
+resource "cloudflare_record" "mail_mx_2" {
+  name    = data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["SECRET_MX_DOMAIN_MX_2"]
+  type    = "MX"
+  ttl     = 300
+  priority = 20
+}
+
 resource "cloudflare_record" "mail_spf" {
   name    = data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
@@ -28,11 +55,30 @@ resource "cloudflare_record" "mail_spf" {
   ttl     = 1
 }
 
-resource "cloudflare_record" "mail_dkey" {
-  name    = "*._domainkey"
+resource "cloudflare_record" "mail_dkey_1" {
+  name    = "protonmail._domainkey"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = data.sops_file.cloudflare_secrets.data["SECRET_MX_DOMAIN_KEY"]
-  type    = "TXT"
+  value   = data.sops_file.cloudflare_secrets.data["SECRET_MX_DOMAIN_KEY_1"]
+  type    = "CNAME"
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_record" "mail_dkey_2" {
+  name    = "protonmail2._domainkey"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["SECRET_MX_DOMAIN_KEY_2"]
+  type    = "CNAME"
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_record" "mail_dkey_3" {
+  name    = "protonmail3._domainkey"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["SECRET_MX_DOMAIN_KEY_3"]
+  type    = "CNAME"
+  proxied = true
   ttl     = 1
 }
 
