b0c45a6db88ae6b21bff2291e2940402aac36cec
Christopher Larivière <lariviere.c@gmail.com>
2022-07-27 08:11:55 -0400

    remove use of deprecated field http3.x
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- provision/terraform/cloudflare/records.tf
+++ provision/terraform/cloudflare/records.tf
@@ -5,7 +5,7 @@ data "http" "ipv4" {
 resource "cloudflare_record" "ip" {
   name    = data.sops_file.cloudflare_secrets.data["SECRET_DOMAIN"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = chomp(data.http.ipv4.body)
+  value   = chomp(data.http.ipv4.response_body)
   proxied = true
   type    = "A"
   ttl     = 1
