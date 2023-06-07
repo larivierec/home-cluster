2d04020d812ffbcc594660a18975ac0c9adca2a5
Christopher Larivière <lariviere.c@gmail.com>
2023-06-07 08:04:21 -0400

    add r2 bucket
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- provision/terraform/cloudflare/account.tf
+++ provision/terraform/cloudflare/account.tf
@@ -1,3 +1,9 @@
 resource "cloudflare_account" "this" {
   name = "garbinc account"
 }
+
+resource "cloudflare_r2_bucket" "backup" {
+  name       = "nas-backup"
+  account_id = cloudflare_account.this.id
+  location   = "ENAM"
+}
