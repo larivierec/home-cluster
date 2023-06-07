94643cc09a40b9558f6b96656d2fb6a9f0ed4416
Christopher Larivière <lariviere.c@gmail.com>
2023-06-07 19:28:47 -0400

    add lifecycle to r2_bucket
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- provision/terraform/cloudflare/account.tf
+++ provision/terraform/cloudflare/account.tf
@@ -6,4 +6,8 @@ resource "cloudflare_r2_bucket" "backup" {
   name       = "nas-backup"
   account_id = cloudflare_account.this.id
   location   = "ENAM"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
