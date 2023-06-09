bc47283422b51c9506753d932dd58ec65512f05a
Christopher Larivière <lariviere.c@gmail.com>
2023-06-09 12:32:16 -0400

    add dns endpoint to ingress controller
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- provision/terraform/cloudflare/cloudflared.tf
+++ provision/terraform/cloudflare/cloudflared.tf
@@ -3,10 +3,3 @@ resource "cloudflare_tunnel" "this" {
   name       = "k3s-tunnel"
   secret     = lookup(local.cloudflare_secrets, "cloudflared_pw").text
 }
-
-resource "cloudflare_record" "cloudflared" {
-  name    = "tunnel"
-  zone_id = data.cloudflare_zone.default.zone_id
-  value   = cloudflare_tunnel.this.cname
-  type    = "CNAME"
-}
