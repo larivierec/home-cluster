26e6409928572e050cd846592d20413279c5ee6e
Christopher Larivière <lariviere.c@gmail.com>
2023-07-28 08:35:33 -0400

    update to use proper private key + add renovate to other gh repo
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- infrastructure/terraform/github/main.tf
+++ infrastructure/terraform/github/main.tf
@@ -36,3 +36,40 @@ module "home-ops" {
     local.default_issue_labels
   )
 }
+
+module "cloudflare-ddns" {
+  source  = "mineiros-io/repository/github"
+  version = "0.18.0"
+
+  name        = "cloudflare-ddns"
+  description = "Basic Cloudflare ddns written in Golang"
+  topics      = ["go", "ddns", "cloudflare"]
+  visibility  = "public"
+
+  auto_init              = false
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+
+  has_issues   = true
+  has_wiki     = false
+  has_projects = false
+  is_template  = false
+
+  plaintext_secrets = {
+    "RIVERBOT_APP_ID"          = lookup(local.github_secrets, "bot_id").text
+    "RIVERBOT_APP_PRIVATE_KEY" = data.bitwarden_item_secure_note.riverbot_private_key.notes
+  }
+
+  issue_labels_merge_with_github_labels = false
+  issue_labels = concat(
+    [
+      { name = "area/ci", color = "72ccf3", description = "Issue relates to CI" },
+      { name = "area/go", color = "72ccf3", description = "Issue relates to Go" },
+
+      { name = "renovate/container", color = "ffc300", description = "Issue relates to a Renovate container update" },
+    ],
+    local.default_issue_labels
+  )
+}
