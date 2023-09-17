7356a0a1d3784e293d11252fc3c45dcfd86e8961
Christopher Larivière <lariviere.c@gmail.com>
2023-09-17 11:59:45 -0400

    fix mongo init
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- kubernetes/apps/database/mongodb/app/config/init-db.js
+++ kubernetes/apps/database/mongodb/app/config/init-db.js
@@ -2,30 +2,30 @@ console.log("MONGO_USER " + process.env.MONGO_USER);
 console.log("MONGO_PASS " + process.env.MONGO_PASSWORD);
 console.log("MONGO_DB " + process.env.MONGO_DBNAME);
 
-db = new Mongo().getDB(process.env.MONGO_DBNAME)
-dbStat = new Mongo().getDB(process.env.MONGO_DBNAME + "_stat")
+dbName = process.env.MONGO_DBNAME;
+dbNameStat = process.env.MONGO_DBNAME + "_stat";
+db = new Mongo().getDB(dbName);
+dbStat = new Mongo().getDB(dbNameStat);
 
 db.createCollection("users", {capped: false});
 dbStat.createCollection("users", {capped: false});
 
-db.createUser(
-  {
-    user: process.env.MONGO_USER, 
-    pwd: process.env.MONGO_PASSWORD, 
-    roles: [
-      {
-        role: "readWrite", db: process.env.MONGO_DBNAME
-      }
-    ]
-  });
+db.createUser({
+  user: process.env.MONGO_USER, 
+  pwd: process.env.MONGO_PASSWORD, 
+  roles: [
+    {
+      role: "readWrite", db: dbName
+    }
+  ]
+});
 
-dbStat.createUser(
-  {
-    user: process.env.MONGO_USER, 
-    pwd: process.env.MONGO_PASSWORD, 
-    roles: [
-      {
-        role: "readWrite", db: process.env.MONGO_DBNAME + "_stat"
-      }
-    ]
-  });
+dbStat.createUser({
+  user: process.env.MONGO_USER, 
+  pwd: process.env.MONGO_PASSWORD, 
+  roles: [
+    {
+      role: "readWrite", db: dbNameStat
+    }
+  ]
+});
