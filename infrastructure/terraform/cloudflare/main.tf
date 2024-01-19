48a0c5fff8c5aa384c77ccbfb96282a39dd0e3a4
Christopher Larivière <lariviere.c@gmail.com>
2024-01-19 13:34:22 -0500

    remove proton
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- infrastructure/terraform/cloudflare/main.tf
+++ infrastructure/terraform/cloudflare/main.tf
@@ -11,18 +11,13 @@ module "dns_records" {
   }
 
   cname_records = {
-    "protonmail._domainkey"  = { cname = "protonmail.domainkey.dpdbrfrsi5bnnbgmawr3gmlslhcmbsffasbqutsxcwjg5hk7ows2a.domains.proton.ch.", ttl = "1", proxied = false },
-    "protonmail2._domainkey" = { cname = "protonmail2.domainkey.dpdbrfrsi5bnnbgmawr3gmlslhcmbsffasbqutsxcwjg5hk7ows2a.domains.proton.ch.", ttl = "1", proxied = false },
-    "protonmail3._domainkey" = { cname = "protonmail3.domainkey.dpdbrfrsi5bnnbgmawr3gmlslhcmbsffasbqutsxcwjg5hk7ows2a.domains.proton.ch.", ttl = "1", proxied = false },
-    "sig1._domainkey"        = { cname = nonsensitive(lookup(local.cloudflare_secrets, "DOMAIN_KEY_APPLE").text), ttl = "3600", proxied = false },
+    "sig1._domainkey" = { cname = nonsensitive(lookup(local.cloudflare_secrets, "DOMAIN_KEY_APPLE").text), ttl = "3600", proxied = false },
   }
 
   mx_records = {
     nonsensitive(lookup(local.cloudflare_secrets, "domain").text) = {
       ttl = "3600"
       records = [
-        { priority = 20, address = "mail.protonmail.ch" },
-        { priority = 30, address = "mailsec.protonmail.ch" },
         { priority = 10, address = "mx01.mail.icloud.com." },
         { priority = 10, address = "mx02.mail.icloud.com." },
       ]
@@ -31,10 +26,8 @@ module "dns_records" {
 
   txt_records = {
     nonsensitive(lookup(local.cloudflare_secrets, "domain").text) = { records = [
-      "protonmail-verification=527b517787bb4a14f0c9160b3355cfb95c1e790e",
       "apple-domain=dPAmcbw67V29ieeV",
-      # "\"v=spf1 include:icloud.com ~all\"",
-      "\"v=spf1 mx include:_spf.protonmail.ch include:icloud.com ~all\"",
+      "v=spf1 include:icloud.com ~all",
     ], ttl = "3600" },
     "_dmarc" = { records = ["v=DMARC1; p=quarantine"], ttl = "1" },
   }
