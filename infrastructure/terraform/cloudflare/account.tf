30504572d6f39f4e86996f9ed4f545a2def7c6d2
Christopher Larivière <lariviere.c@gmail.com>
2024-01-19 17:03:59 -0500

    enforce 2fa
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- infrastructure/terraform/cloudflare/account.tf
+++ infrastructure/terraform/cloudflare/account.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_account" "this" {
-  name = "garbinc account"
-
+  name              = "garbinc account"
+  enforce_twofactor = true
   lifecycle {
     prevent_destroy = true
   }
