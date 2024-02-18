e780c1755249f8ca12ef332be8c4c92a4e9ec470
Christopher Larivière <lariviere.c@gmail.com>
2024-02-18 00:45:44 -0500

    Revert "chore(deps): update terraform github to v6 (#2886)" (#2898)
    This reverts commit 170e5c136549a3c7a728ab136f94f7b08fd0f6c6.

--- infrastructure/terraform/github/requirements.tf
+++ infrastructure/terraform/github/requirements.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "6.0.0"
+      version = "5.45.0"
     }
     sops = {
       source  = "carlpett/sops"
