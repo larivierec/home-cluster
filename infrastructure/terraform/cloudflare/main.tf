d74e7c9322f61b217b1755ee98f2bc91629ba6dc
Christopher Larivière <lariviere.c@gmail.com>
2024-02-27 10:45:48 -0500

    add dmarc reports
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- infrastructure/terraform/cloudflare/main.tf
+++ infrastructure/terraform/cloudflare/main.tf
@@ -34,7 +34,7 @@ module "dns_records" {
       "apple-domain=dPAmcbw67V29ieeV",
       "v=spf1 include:icloud.com ~all",
     ], ttl = "3600" },
-    "_dmarc" = { records = ["v=DMARC1; p=quarantine"], ttl = "1" },
+    "_dmarc" = { records = ["v=DMARC1; p=quarantine rua=mailto:baee06f019b04b0cacb868a9de6a7ade@dmarc-reports.cloudflare.net"], ttl = "1" },
   }
 }
 
