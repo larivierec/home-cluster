c480b99fd09e76129357e6a7388e4fe38dbb1c4d
Christopher Larivière <lariviere.c@gmail.com>
2024-10-17 08:49:42 -0400

    start adding flux image tester
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- infrastructure/terraform/github/main.tf
+++ infrastructure/terraform/github/main.tf
@@ -22,6 +22,7 @@ module "home-cluster" {
   plaintext_secrets = {
     "RIVERBOT_APP_ID"          = nonsensitive(local.github_secrets["bot_id"])
     "RIVERBOT_APP_PRIVATE_KEY" = nonsensitive(base64decode(local.github_secrets["bot_pk_b64"]))
+    "BW_ACCESS_TOKEN"          = nonsensitive(local.github_secrets["gha_bw_access_token"])
   }
 
   issue_labels_merge_with_github_labels = false
