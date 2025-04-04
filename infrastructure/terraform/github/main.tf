70aa2cca06f28f2ceaa29a57b82c5a1c330d393a
Christopher Larivière <lariviere.c@gmail.com>
2025-04-04 09:42:48 -0400

    add branch default
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- infrastructure/terraform/github/main.tf
+++ infrastructure/terraform/github/main.tf
@@ -158,3 +158,8 @@ resource "github_actions_secret" "bot_app_pk" {
   secret_name     = "RIVERBOT_APP_PRIVATE_KEY"
   plaintext_value = nonsensitive(base64decode(local.github_secrets["bot_pk_b64"]))
 }
+
+resource "github_branch_default" "name" {
+  branch     = "main"
+  repository = github_repository.renovate.name
+}
