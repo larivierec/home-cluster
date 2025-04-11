ccdc6746e9b9b5dfe7cdb86e1b5c536c119e8fbe
Christopher Larivière <lariviere.c@gmail.com>
2025-04-10 20:45:51 -0400

    migrate to minio state
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- infrastructure/terraform/cloudflare/zone.tf
+++ infrastructure/terraform/cloudflare/zone.tf
@@ -4,41 +4,6 @@ data "cloudflare_zone" "default" {
   name       = "garb.dev"
 }
 
-resource "cloudflare_zone_settings_override" "cloudflare_settings" {
-  zone_id = data.cloudflare_zone.default.zone_id
-  settings {
-    # ssl
-    always_use_https = "on"
-    ssl              = "strict"
-
-    min_tls_version          = "1.2"
-    opportunistic_encryption = "on"
-    tls_1_3                  = "zrt"
-    automatic_https_rewrites = "on"
-    universal_ssl            = "on"
-
-    # site settings
-    security_level = "medium"
-    brotli         = "on"
-    rocket_loader  = "off"
-
-    # network
-    http3               = "on"
-    ipv6                = "on"
-    websockets          = "on"
-    pseudo_ipv4         = "off"
-    always_online       = "off"
-    ip_geolocation      = "on"
-    zero_rtt            = "on"
-    opportunistic_onion = "on"
-
-    # scrape shield
-    email_obfuscation   = "on"
-    server_side_exclude = "on"
-    hotlink_protection  = "on"
-  }
-}
-
 data "http" "uptimerobot_ipv4" {
   url = "https://uptimerobot.com/inc/files/ips/IPv4.txt"
 }
--- infrastructure/terraform/github/requirements.tf
+++ infrastructure/terraform/github/requirements.tf
@@ -1,9 +1,18 @@
 terraform {
-  backend "remote" {
-    organization = "larivierec"
-    workspaces {
-      name = "home-github-provisioner"
+  backend "s3" {
+    bucket = "terraform"
+    key    = "github/github.tfstate"
+    region = "main"
+
+    endpoints = {
+      s3 = "https://s3.garb.dev"
     }
+
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true
   }
 
   required_providers {
